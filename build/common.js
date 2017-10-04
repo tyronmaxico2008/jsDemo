f8f6404e3c1c008ea635c671e37aae63b7f786ed
tyronmaxico2008 <amitjha211@outlook.com>
2017-10-04 15:08:28 +0530

    test application Deployed

--- build/common.js
+++ build/common.js
@@ -84,3 +84,54 @@ function clsAjaxProcessing(e) {
         }
     }
 }
+
+
+//"http://localhost/web_test/test/test_post"
+
+function clsMyAjax(sUrl) {
+    var xhr = new XMLHttpRequest();
+    
+    var self = this;
+
+    xhr.upload.addEventListener("progress", function(evt){
+      if (evt.lengthComputable) {
+        //console.log("add upload event-listener" + evt.loaded + "/" + evt.total);
+        var iPer = (evt.loaded * 100 ) / evt.total;
+        self.onProgress(iPer);
+      }
+    }, false);
+    
+    /*
+    xhr.onprogress = function (e) {
+        debugger;
+        if (e.lengthComputable) {
+            console.log(e.loaded+  " / " + e.total)
+        }
+    }
+    */
+    this.onProgress = function(per){
+        console.log(per);
+    }
+    xhr.onloadstart = function (e) {
+        console.log("start")
+    }
+    xhr.onloadend = function (e) {
+        debugger;
+        console.log("end")
+    } 
+    
+    
+    var file = document.getElementById("txtFile");
+    //var fileData = ;
+    
+    this.send = function(sType,data){
+        xhr.open(sType, sUrl);
+        xhr.send(data);
+    }
+
+    
+    this.post = function(oData,callBack){
+        self.send("POST",oData);
+    }
+    
+}
